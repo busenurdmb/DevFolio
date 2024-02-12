ccefdbe1746eb107d9f28959f80f7af2d8aefd24
busenurdmb <bus.dmb@gmail.com>
2024-02-12 18:10:13 +0300

    Add project files.

---
+++ DevFolio/Controllers/ContactController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevFolio.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SendMessage()
+        {
+            return View();
+        }
+
+        public ActionResult MessageList()
+        {
+            return View();
+        }
+    }
+}
+//camelCase->sade baş harf küçük
+//pascalCase->başharf büüyk
+//Snakecase->hepsi küçük
+//deişkenlerde-metotler-filek-property-ınterface-
