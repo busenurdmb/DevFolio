058a8a0731f282003b3b59296e9e60a92d244637
busenurdmb <bus.dmb@gmail.com>
2024-02-14 20:30:56 +0300

    statistic controller oluşturuldu. bir kaç istatik çekildi

---
+++ DevFolio/Controllers/StatisticController.cs
@@ -0,0 +1,24 @@
+﻿using DevFolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevFolio.Controllers
+{
+    public class StatisticController : Controller
+    {
+        DbDevFolioEntities db = new DbDevFolioEntities();
+        public ActionResult Index()
+        {
+            ViewBag.CategoryCount = db.TblCategory.Count();
+            ViewBag.ProjeCount = db.TblProject.Count();
+            ViewBag.SkillCount = db.TblSkill.Count();
+            ViewBag.SkillavgValue = db.TblSkill.Average(x => x.SkillValue);
+            ViewBag.SkillLastTitle = db.GetLastSkillTitle().FirstOrDefault();
+            ViewBag.coreCategoryProjectCount = db.TblProject.Where(x => x.ProjectCategory == 1).Count();
+            return View();
+        }
+    }
+}
